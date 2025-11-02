f468d2151c8d18873199d360fee593abc58095f2
Jay Chauhan <jcha0338@student.monash.edu>
2025-11-02 15:22:37 +1100

    Updated the stratified data to have factors leveled

--- data-raw/hai_data.R
+++ data-raw/hai_data.R
@@ -77,7 +77,10 @@ bhai_strata_summary <- bhai_summary_df |>
     # Clean up age group formatting
     age_group = str_replace_all(age_group, "Inf", "120"),
     age_group = str_replace_all(age_group, "\\[\\s*([+-]?\\d+)\\s*;\\s*([+-]?\\d+)\\s*\\]", "\\1-\\2"),
-    age_group = as.factor(age_group)
+    age_group = factor(age_group, levels = c("0-0","1-4","5-9","10-14","15-19",
+                                             "20-24","25-29","30-34","35-39","40-44",
+                                             "45-49","50-54","55-59","60-64","65-69",
+                                             "70-74","75-79","80-84","85-120"))
   )
 
 # ---- Dataset 3: Total Population Estimates ----
